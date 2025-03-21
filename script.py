fcb716eca381dfc9fe13ad4ff7d3fa77984e9f3a
nyzd <azerothadev@gmail.com>
2025-03-21 19:58:23 +0330

    recive Mushaf info from user

--- script.py
+++ script.py
@@ -6,13 +6,13 @@ import xml.etree.ElementTree as ET
 from lib.quran import Quran, Mushaf
 import json
 
-def export_quran(file_path, pretty = False):
+def export_quran(file_path, mushaf_id, mushaf_name, mushaf_full_name, mushaf_source,pretty = False,):
     with open(file_path, 'r') as file:
         content = file.read().encode("utf-8")
 
     root = ET.fromstring(content)
 
-    quran = Quran(Mushaf(1, "hafs", "hafs", "tanzil")).surahs_from_xml(root)
+    quran = Quran(Mushaf(mushaf_id, mushaf_name, mushaf_full_name, mushaf_source)).surahs_from_xml(root)
 
     result = json.dumps(quran, default=vars, ensure_ascii=False, indent=(pretty and 4) or None)
 
@@ -24,8 +24,12 @@ def main(args):
     command = args[1]
     match command:
         case "quran":
-            pretty = (args[3] == "--pretty") if len(args) > 3 else False
-            export_quran(args[2], pretty)
+            mushaf_id = int(args[3])
+            mushaf_name = args[4]
+            mushaf_full_name = args[5]
+            mushaf_source = args[6]
+            pretty = (args[7] == "--pretty") if len(args) > 6 else False
+            export_quran(args[2], mushaf_id, mushaf_name, mushaf_full_name, mushaf_source,pretty)
 
 if __name__ == "__main__":
     main(sys.argv)
